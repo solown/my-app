2335fb22cac9420f7a8c2d61de92fff12fc370bd
solown <thibaudrider@live.fr>
2020-06-10 14:14:30 +0200

    Including new graph

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,35 +16,61 @@ export class HomeComponent implements OnInit {
   myModel : any;
   LineChart = null;
   constructor(private JSONPlaceholder:JSONPlaceholderService){}
+
+  ngOnInit(){ }
+
+
   getDataFromApi(){
-    let year = [];
-    let birth_boy = [];
-    let birth_girl = [];
-    
-    var strAnnee;
-    var debutAnnee = this._currentYears[0];
-    var finAnnee = this._currentYears[1];
-    console.log(debutAnnee);
-    console.log(finAnnee);
-    var i;
-    for (i = debutAnnee; i < finAnnee; i++){
-      strAnnee = i.toString()
+    console.log(this._currentYears[0])
+    console.log(this._currentYears[1])
+    if (this._currentYears[0]==this._currentYears[1]){
+      let birth_boy = 0;
+      let birth_girl = 0;
+      strAnnee = this._currentYears[0].toString()
       this.JSONPlaceholder.getData(null, strAnnee).subscribe((data) => {
         
-        birth_boy.push(data['records'][0]["fields"]["garcons"]);
-        birth_girl.push(data['records'][0]["fields"]["filles"])
+        birth_boy = data['records'][0]["fields"]["garcons"];
+        birth_girl = data['records'][0]["fields"]["filles"];
       })
-      year.push(i);
-    }
-    if (this.LineChart !== null){
-      this.LineChart.destroy();
-      this.displayCharts(birth_boy,birth_girl, year);
+      if (this.LineChart !== null){
+        console.log("Goes here");
+        this.LineChart.destroy();
+        this.displayPieChart(birth_boy,birth_girl, this._currentYears[0]);
+      }
+      else{
+        console.log("Goes here else");
+        this.displayPieChart(birth_boy,birth_girl, this._currentYears[0]);
+      }
     }
     else{
-      this.displayCharts(birth_boy,birth_girl, year);
+      let year = [];
+      let birth_boy = [];
+      let birth_girl = [];
+      var strAnnee;
+      var debutAnnee = this._currentYears[0];
+      var finAnnee = this._currentYears[1];
+      var i;
+      for (i = debutAnnee; i < finAnnee; i++){
+        strAnnee = i.toString()
+        this.JSONPlaceholder.getData(null, strAnnee).subscribe((data) => {
+          
+          birth_boy.push(data['records'][0]["fields"]["garcons"]);
+          birth_girl.push(data['records'][0]["fields"]["filles"])
+        })
+        year.push(i);
+      }
+      if (this.LineChart !== null){
+        this.LineChart.destroy();
+        this.displayCharts(birth_boy,birth_girl, year);
+      }
+      else{
+        this.displayCharts(birth_boy,birth_girl, year);
+      }
     }
     
   }
+
+
   displayCharts(birth_boy : number[], birth_girl : number[], year : number[]){
     var content;
     var valueCb = this.checkBoxContent();
@@ -103,29 +129,46 @@ export class HomeComponent implements OnInit {
           yAxes: [{
             display: true,
             tick:{
-              beginAtZero : false
-              /*    
-              https://www.chartjs.org/docs/latest/developers/updates.html
-              var hi = data.datasets[3].data[0];
-              var lo = data.datasets[1].data[data.datasets[1].data.length - 1];
-                              ticks: {
-                                  max: hi,
-                                  min: lo,
-                                  stepSize: 10
-                                  }*/
-                            
+              beginAtZero : false                           
             }
           }]
         }
       }
     })
   }
-
-  ngOnInit(){  
+  displayPieChart(birth_boy : number,birth_girl : number,  year : number){
+    this.LineChart = new Chart('canvas',{
+      type : 'pie',
+      data: {
+        labels: ["Naissance garçons en " +year, "Naissance filles en " + year],
+        datasets : [{
+         /* data: [120,80],
+          backgroundColor : [
+            'rgb(255, 60, 55,0)',
+            'rgba(105, 0, 255,0)'
+          ]*/
+            label : "Naissance filles depuis "+this._currentYears[0].toString() + " à " + this._currentYears[1].toString(),
+            data:birth_girl,
+            borderColor: '#ffcc00',
+        },
+        {
+          label : "Naissance garcons depuis "+ this._currentYears[0].toString() + " à " + this._currentYears[1].toString(),
+          data: birth_boy,
+          borderColor: '#3cba9f',
+        }
+      ]
+      },
+      options: {
+        legend: {
+          text: "Pie Chart",
+          display: true,
+          maintainAspectRatio: false
+        }
+      }
+    })
   }
-
+  
 checkBoxContent(){
-  var choix;
   let checkBoxBoys = (<HTMLInputElement>document.getElementById("keeplog")).checked;
   let checkBoxGirls = (<HTMLInputElement>document.getElementById("keeplog2")).checked;
   if (checkBoxGirls == true && checkBoxBoys == true)
